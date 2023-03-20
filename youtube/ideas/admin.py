75f34a46bb36721eec4bfe8efc5cab39008be1c3
Hub <hubert@hms-tech.pl>
2023-03-20 10:46:10 +0100

    16:07

--- youtube/ideas/admin.py
+++ youtube/ideas/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Idea, Vote
 
-# Register your models here.
+
+@admin.register(Idea)
+class IdeaAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+    list_display = ['title', 'status', 'youtube_url']
+    list_filter = ['status']
+
+    def show_youtube_url(self, obj):
+        return 'Tu będzie URL'
+@admin.register(Vote)
+class VoteAdmin(admin.ModelAdmin):
+    list_display = ['id', 'idea', 'reason']
+    list_filter = ['idea']
--- youtube/ideas/models.py
+++ youtube/ideas/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 
+IDEA_STATUS = (
+    ('pending', 'Waiting for review'),
+    ('accepted', 'Accepted'),
+    ('done', 'Done'),
+    ('rejected', 'Rejected')
+)
+
 
 class Idea(models.Model):
-    title - models.CharField(max_length=255)
-    description - models.TextField()
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    youtube_url = models.URLField(null=True, blank=True)
+    status = models.CharField(choices=IDEA_STATUS, max_length=30, default= "pending")
 
+    def __str__(self):
+        return self.title
 
 class Vote(models.Model):
-    Idea = models.ForeignKey(Idea, on_delete=models.CASCADE)
-
+    idea = models.ForeignKey(Idea, on_delete=models.CASCADE)
+    reason = models.TextField()
