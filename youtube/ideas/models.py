21426fffc0c6587ad8046d929d8015c781a8d6d6
Hub <hubert@hms-tech.pl>
2023-03-20 10:46:10 +0100

    10:46

--- youtube/ideas/models.py
+++ youtube/ideas/models.py
@@ -4,8 +4,9 @@ from django.db import models
 class Idea(models.Model):
     title - models.CharField(max_length=255)
     description - models.TextField()
+    youtube_url = models.URLField()
 
-
-class Vote(models.Model):
-    Idea = models.ForeignKey(Idea, on_delete=models.CASCADE)
+    class Vote(models.Model):
+    idea = models.ForeignKey(Idea, on_delete=models.CASCADE)
+    reason = models.TextField()
 
